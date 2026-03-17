f18f86f71850fad76b024f1dffe2d1d83e10d3f1
Carlos Narváez <narvaezcarlos88@gmail.com>
2026-03-17 03:56:20 +0000

    reduced code from example in class

---
+++ src/js/components/CardMenu.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const CardMenu = (props) => {
+    return (
+        <div className="card mx-5 mb-5">
+            <div className="card-body">
+                <h1 className="card-title">A warn welcome!</h1>
+                <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" className="btn btn-primary">Call to action!</a>
+            </div>
+        </div>
+    )
+}
+
